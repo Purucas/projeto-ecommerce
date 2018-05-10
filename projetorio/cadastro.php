e7d0319091dc0730d1e1213acfe5ce89abff8c3b
Roberto Accacio <purucabolinho@hotmail.com>
2018-05-09 21:22:36 -0300

    site atualizado

--- projetorio/cadastro.php
+++ projetorio/cadastro.php
@@ -99,6 +99,18 @@
       <input type="nome" class="form-control" id="nome" placeholder="Digite o seu nome">
     </div>
   </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="endereco">Endereço:</label>
+    <div class="col-sm-10">
+      <input type="endereco" class="form-control" id="endereco" placeholder="Digite seu endereço">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="email">Email:</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="email" placeholder="Enter email">
+    </div>
+  </div>
     <div class="form-group">        
       <div class="col-sm-offset-2 col-sm-10">
         <div class="checkbox">
